a10e9875a2bde2a4249845031af9c1ba2d22f5bb
gowtham arya <gowtham.arya999@gmail.com>
2023-12-07 20:33:53 +0530

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # rejouice
-# rejouice
+# Animated Website GitHub Project
+
+Welcome to our Animated Website GitHub project, a platform where creativity meets technology to deliver captivating and dynamic web experiences. Elevate your web development skills with a curated collection of animations, seamlessly crafted to bring your websites to life.
+
+## Features
+
+- *GSAP Integration:* Leverage the power of GreenSock Animation Platform (GSAP) to create smooth, high-performance animations for visually stunning and user-friendly websites.
+
+- *SwiperJS for Carousel Magic:* Integrate SwiperJS, a modern touch slider, to add interactivity and create captivating carousels and sliders.
+
+- *Versatile Animation Library:* Explore a variety of animations, from subtle fades to eye-catching transitions, catering to different design needs and preferences.
+
+- *Easy Customization:* Tailor animations with ease. Our project is designed for simplicity, allowing developers of all levels to integrate and customize animations seamlessly.
+
+## Technologies Used
+
+- *HTML5 & CSS3:* Modern web technologies providing a semantic structure, style, and responsiveness.
+
+- *Node Package Manager (NPM):* Efficiently manage project dependencies for a streamlined development process.
+
+- *Webpack for Bundling:* Bundle and optimize your code, ensuring optimal performance and faster loading times.
+
+- *Babel for Cross-Browser Compatibility:* Write modern JavaScript with confidence, ensuring compatibility across different browsers.
+
+## Getting Started
+
+1. Clone the repository: git clone https://github.com/GowthamArya/rejouice.git
+
+2. Customize animations and styles according to your project requirements.
+
+3. Build and deploy your animated website.
+
+## Contributing
+
+We welcome contributions! If you have ideas for improvements or new features, feel free to open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+Join us on this journey of creativity and innovation. Unleash the potential of GSAP, SwiperJS, and more to create a web experience that goes beyond static pages, leaving a lasting impression on your users.# rejouice
---
+++ script.js
@@ -0,0 +1,111 @@
+function sliderAnimation(){
+  var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 3,
+      spaceBetween: 10,
+      loop: true,
+      autoplay: {
+        delay: 1500,
+        disableOnInteraction: false,
+      },
+  });
+}
+sliderAnimation()
+
+
+function locoScroll () {
+    gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+
+}
+locoScroll();
+
+function cursorEffect() {
+    var page1Content = document.querySelector("#page1-content");
+var cursor = document.querySelector("#cursor");
+page1Content.addEventListener("mousemove", function(dets){
+    gsap.to(cursor,{
+        x:dets.x,
+        y:dets.y
+    })
+})
+page1Content.addEventListener("mouseenter",function(){
+    gsap.to(cursor,{
+        scale:1,
+        opacity:1,
+    });
+})
+page1Content.addEventListener("mouseleave",function(){
+    gsap.to(cursor,{
+        scale:0,
+        opacity:0
+    });
+})
+}
+cursorEffect();
+function page2Animation(){
+    gsap.from(".elem h1",{
+        y:100,
+        stagger:0.2,
+        duration:1,
+        scrollTrigger:{
+            trigger:"#page2",
+            scroller:"#main",
+            start:"top 47%",
+            end:"top 45%",
+            // markers:true,
+            scrub:1
+        }
+    })
+}
+page2Animation();
+
+var tl = gsap.timeline()
+
+tl.from("#loader h3",{
+  x:40,
+  opacity:0,
+  duration:1,
+  stagger:0.2
+})
+tl.to("#loader h3",{
+  x:-40,
+  opacity:0,
+  duration:1,
+  stagger:-0.2
+})
+tl.to("#loader",{
+  opacity:0,
+  display:"none",
+})
+tl.from("#page1-content span",{
+  y:100,
+  opacity:0,
+  stagger:0.1
+})
